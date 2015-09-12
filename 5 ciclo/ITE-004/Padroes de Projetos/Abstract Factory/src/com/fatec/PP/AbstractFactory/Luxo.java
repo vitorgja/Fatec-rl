eb780a93d601cceab4da2867ba418933bb31e9c4
Vitor Pereira <vitor_gja_@hotmail.com>
2015-09-11 23:43:19 -0300

    add Abstract Factory

---
+++ 5 ciclo/ITE-004/Padroes de Projetos/Abstract Factory/src/com/fatec/PP/AbstractFactory/Luxo.java
@@ -0,0 +1,10 @@
+package com.fatec.PP.AbstractFactory;
+
+public class Luxo extends Carro {
+
+	Luxo(String nome, Localidade localidade) {
+		super(nome, localidade);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
